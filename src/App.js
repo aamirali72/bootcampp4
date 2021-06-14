1547a85e5d06d83f878a3e3c4bdabc294f60c7d6
aamirali <sufaamir@gmail.com>
2021-06-14 14:07:23 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Aamir Ali</code> and save to reload.
+          <code>Aamir Ali</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
